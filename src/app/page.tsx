d807f7a8dd0f91818cef0c9746549160d9eafa30
Julian Ramos <jramostod@gmail.com>
2024-07-19 13:43:52 -0300

    top bar skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import TopBar from "../components/TopBar";
 import TypeWritter from "../components/TypeWritter";
 
 export default function Home() {
   return (
     <main>
+      <div>
+        <TopBar />
+      </div>
       <div className="flex justify-center items-center mt-12">
         <TypeWritter />
       </div>
---
+++ src/components/TopBar.tsx
@@ -0,0 +1,35 @@
+export default function TopBar() {
+  return (
+    <div className="flex justify-between items-center p-4 bg-gray-800 text-white">
+      <div>
+        <h1 className="text-2xl font-semibold">Julián Ramos</h1>
+      </div>
+      <div>
+        <button
+          type="button"
+          className="text-white font-semibold py-2 px-4 rounded"
+        >
+          About me
+        </button>
+        <button
+          type="button"
+          className="text-white font-semibold py-2 px-4 rounded"
+        >
+          Work Experience
+        </button>
+        <button
+          type="button"
+          className="text-white font-semibold py-2 px-4 rounded"
+        >
+          Projects
+        </button>
+        <button
+          type="button"
+          className="text-white font-semibold py-2 px-4 rounded"
+        >
+          Contact
+        </button>
+      </div>
+    </div>
+  );
+}
