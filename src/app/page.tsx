0e34d1504fda478eff5dbc005572d79e7d1818ea
Julian Ramos <jramostod@gmail.com>
2024-07-20 16:42:32 -0300

    fixes some screen issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
 "use client";
 
 import Projects from "../components/Projects";
-import TopBar from "../components/TopBar";
 import TypeWritter from "../components/TypeWritter";
 import WorkExperience from "../components/WorkExperience";
 
 export default function Home() {
   return (
-    <main className="h-full bg-blue-950">
-      <section>
-        <TopBar />
-      </section>
+    <main className="flex flex-col h-screen bg-blue-950">
       <section className="flex justify-center items-center mt-12">
         <TypeWritter />
       </section>
-      <section className="flex justify-between mt-12">
+      <section className="flex justify-between mt-12 flex-grow">
         <section className="w-full">
           <WorkExperience />
         </section>
-        <div className="border-r border-white h-[720px] border-x-4" />
+        <div className="border-r border-white border-x-4" />
         <section className="w-full">
           <Projects />
         </section>
--- src/components/TopBar.tsx
+++
@@ -1,9 +0,0 @@
-export default function TopBar() {
-  return (
-    <div className="flex justify-between items-center p-4 text-white">
-      <div>
-        <h1 className="text-2xl font-semibold">Julián Ramos Portfolio</h1>
-      </div>
-    </div>
-  );
-}
