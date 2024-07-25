80778bbb6d55dc83d6f3b19533e48457ee3d4b4b
Julian Ramos <jramostod@gmail.com>
2024-07-24 21:46:11 -0300

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect, useRef, RefObject } from "react";
 import Projects from "../components/Projects";
 import WorkExperience from "../components/WorkExperience";
 import Index from "../components/Index";
@@ -9,19 +9,64 @@ import AnimatedTypeWritter from "../components/AnimatedTypeWritter";
 import AnimatedPageContent from "../components/AnimatedPageContent";
 import Contact from "../components/Contact";
 
+interface SectionRefs {
+	[key: string]: HTMLElement | null;
+}
+
 export default function Home() {
 	const [showContent, setShowContent] = useState(false);
+	const [activeSection, setActiveSection] = useState<string | null>(null);
+	const sectionRefs = useRef<SectionRefs>({
+		about: null,
+		work: null,
+		projects: null,
+		contact: null,
+	});
+
+	useEffect(() => {
+		const observerOptions = {
+			root: null,
+			rootMargin: "0px",
+			threshold: 0.5,
+		};
+
+		const observerCallback = (entries: IntersectionObserverEntry[]) => {
+			console.log({ entries });
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					console.log({ entry });
+					setActiveSection(entry.target.id);
+				}
+			});
+		};
+
+		const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+		Object.values(sectionRefs.current).forEach((ref) => {
+			if (ref) {
+				observer.observe(ref);
+			}
+		});
+
+		return () => {
+			Object.values(sectionRefs.current).forEach((ref) => {
+				if (ref) {
+					observer.unobserve(ref);
+				}
+			});
+		};
+	}, []);
 
 	return (
 		<main className="relative flex flex-col bg-cover bg-fixed bg-center min-h-screen bg-gradient-to-b from-slate-950 to-slate-900">
 			<AnimatedTypeWritter onFinish={() => setShowContent(true)} />
 			{showContent && (
 				<AnimatedPageContent>
-					<section className="flex mt-36" id="body">
+					<section className="flex mt-48" id="body">
 						<section className="flex flex-col flex-grow">
 							<div className="sticky top-0">
-								<div className="flex justify-center mt-80">
-									<Index />
+								<div className="flex justify-center mt-72">
+									<Index activeSection={activeSection} />
 								</div>
 							</div>
 						</section>
@@ -29,20 +74,20 @@ export default function Home() {
 						<section className="flex columns-6">
 							<section className="flex flex-col gap-20">
 								<section className="flex justify-center">
-									<div className="w-[750px]">
+									<div className="w-[750px]" id="about" ref={(el) => (sectionRefs.current.about = el)}>
 										<About />
 									</div>
 								</section>
-								<section>
+								<section id="work" ref={(el) => (sectionRefs.current.work = el)}>
 									<WorkExperience />
 								</section>
-								<section>
+								<section id="projects" ref={(el) => (sectionRefs.current.projects = el)}>
 									<Projects />
 								</section>
 							</section>
 						</section>
 					</section>
-					<section className="mt-24">
+					<section className="mt-24" id="contact" ref={(el) => (sectionRefs.current.contact = el)}>
 						<Contact />
 					</section>
 				</AnimatedPageContent>
--- src/components/Index.tsx
+++ src/components/Index.tsx
@@ -1,15 +1,22 @@
 type IndexItemProps = {
 	label: string;
 	onClick: () => void;
+	active: boolean;
+};
+
+type IndexProps = {
+	activeSection: string | null;
 };
 
 function Bar() {
 	return <div className="flex-grow border-t rounded-xl border-gray-500 border-y-2" />;
 }
 
-function Item({ label, onClick }: IndexItemProps) {
+function Item({ label, onClick, active }: IndexItemProps) {
 	return (
-		<div className="flex gap-4 items-center transition-transform duration-200 ease-in-out hover:scale-125">
+		<div
+			className={`flex gap-4 items-center transition-transform duration-200 ease-in-out ${active ? "scale-125 font-bold" : "hover:scale-125"}`}
+		>
 			<Bar />
 			<div className="w-[60%]">
 				<button
@@ -24,13 +31,19 @@ function Item({ label, onClick }: IndexItemProps) {
 	);
 }
 
-export default function Index() {
+export default function Index({ activeSection }: IndexProps) {
+	const items = [
+		{ label: "About", onClick: () => console.log("About") },
+		{ label: "Experience", onClick: () => console.log("Experience") },
+		{ label: "Projects", onClick: () => console.log("Projects") },
+		{ label: "Contact", onClick: () => console.log("Contact") },
+	];
+
 	return (
 		<div className="flex w-48 flex-col gap-2">
-			<Item label="About" onClick={() => console.log("About")} />
-			<Item label="Experience" onClick={() => console.log("Experience")} />
-			<Item label="Projects" onClick={() => console.log("Projects")} />
-			<Item label="Contact" onClick={() => console.log("Contact")} />
+			{items.map((item) => (
+				<Item key={Math.random() * 1000} {...item} active={activeSection?.toLowerCase() === item.label.toLowerCase()} />
+			))}
 		</div>
 	);
 }
